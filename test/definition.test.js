cafdb0f744eae6314f330fe612d96bcb724812f6
1d82fdaaf812 <dmartinezg@psl.com.co>
2016-10-12 16:44:44 -0500

    Add base files

---
+++ test/definition.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const definition = require('../lib/definition.json');
+
+describe('Gerundio', () => {
+
+  it('shoud have models', () => {
+    
+  })
+
+  it('should have primitive', () => {
+
+  })
+
+  it('should have keywords', () => {
+
+  })
+
+  it('should have comparators', () => {
+
+  })
+
+})
